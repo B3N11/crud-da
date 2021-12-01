4be32d79f2c716ac2978ddfa913ce622eeac7764
B3NII <89522330+B3N11@users.noreply.github.com>
2021-12-01 23:37:19 +0100

    Starting Project
    - Add notes
    - Create Server Project
    - Import & modify NetClient and NetClientController
    - Create base of User and UserData class
    - Create Logger class

---
+++ Programs/Server/CarCRUDServer/Server.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CarCRUD
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
